b4aba3b15eedf6e4a304734ba27e6de5ae16c61a
talia <taliaberrio9@gmail.com>
2023-09-30 23:07:58 -0500

    proyecto intehrador 1

---
+++ main.py
@@ -0,0 +1,8 @@
+# Pedir el nombre del jugador por teclado
+nombre_jugador = input("Por favor, introduce tu nombre: ")
+
+# Imprimrir un mensage de bienvenida
+print ("¡Hola," + " " + nombre_jugador +"!")
+
+
+
